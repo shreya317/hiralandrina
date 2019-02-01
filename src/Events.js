e41a1829124981b4324717690a315b8b4c543964
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2019-02-01 17:40:42 -0600

    wip

--- src/Events.js
+++ src/Events.js
@@ -7,14 +7,14 @@ export const Events = () => (
         <h1 className='Events-header'>Events</h1>
         <div className='Events-sangeet'>
             <div className='Events-desc'>
-               <div className='Event-text'>
-               <p>Thursday, July 4, 2019</p>
+                <div className='Event-title'><h2>Sangeet</h2></div>
+                <div className='Event-text'>
+                <p>Thursday, July 4, 2019</p>
                 <p>6:30 PM</p>
                 <p>Lincolnshire Marriott Resort</p>
                 <p>10 Marriott Dr, Lincolnshire, IL 60069</p>
                 <button className='Events-button'><a href='https://www.google.com/maps/dir//Lincolnshire+Marriott+Resort,+10+Marriott+Dr,+Lincolnshire,+IL+60069/@42.1964489,-87.9279083,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fbe74451739a1:0x85f9448b840f0781!2m2!1d-87.9257196!2d42.1964449!3e0' target='_blank'>Directions</a></button>
                </div>
-               <div className='Event-title'><h2>Sangeet</h2></div>
             </div>
         </div>
         <div className='Events-wedding'>
@@ -32,6 +32,7 @@ export const Events = () => (
         </div>
         <div className='Events-reception'>
             <div className='Events-desc'>
+                <div className='Event-title'><h2>Reception</h2></div>
                 <div className='Event-text'>
                 <p>Saturday, July 6, 2019</p>
                 <p>6:00 PM Cocktails</p>
@@ -40,7 +41,6 @@ export const Events = () => (
                 <p>100 Drury Ln, Oakbrook Terrace, IL 60181</p>
                 <button className='Events-button'><a href='https://www.google.com/maps/dir//Drury+Lane+Theatre,+100+Drury+Ln,+Oakbrook+Terrace,+IL+60181/@41.8595634,-87.9547528,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880e4c5fd17b0c3b:0x8b0ebd9262d219a7!2m2!1d-87.9525588!2d41.8595594!3e0' target='_blank'>Directions</a></button>
                 </div>
-                <div className='Event-title'><h2>Reception</h2></div>
             </div>
         </div>
     </div>  
