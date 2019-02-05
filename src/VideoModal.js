a33b91821e982ec5741cf00f550ad3d09d5da1cc
Shreya Patel <shreyapatel@Shreya-Patels-MacBook-Pro.local>
2019-02-05 14:17:51 -0600

    update to video iframe

--- src/VideoModal.js
+++ src/VideoModal.js
@@ -12,12 +12,13 @@ export default class VideoModal extends Component {
            <div className='VideoModal'>
                 <button className='VideoModal-button' onClick={() => this.handleClick()} >Enter Rina & Hiral's Wedding Website</button>
                 <iframe 
-                    className='VideoModal-video' 
-                    title='video' 
-                    src="https://drive.google.com/file/d/1gC5lL9RQkOixqTIDxHbQyOkFZ-Z23BCm/preview" 
+                    className='VideoModal-video'
+                    title={'video'}
                     height='900' 
-                    width='100%'
-                    frameborder='0'
+                    width='100%' 
+                    src="https://www.youtube-nocookie.com/embed/0h_bB7dqNso?rel=0&amp;showinfo=0" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
                 >
                     Video
                 </iframe>
