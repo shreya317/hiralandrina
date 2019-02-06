59383ea69c65da00ee5e96363d75b5c97ac3adc7
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2019-02-06 10:14:49 -0600

    add missing info

--- src/Accommodations.js
+++ src/Accommodations.js
@@ -12,10 +12,18 @@ export const Accommodations = () => (
                 <p>10 Drury Ln, Oak Brook Terrace, Illinois 60181</p>
                 <p><button className='Accommodations-button'><a href='https://www.google.com/maps/dir//Hilton+Garden+Inn+Chicago%2FOakbrook+Terrace,+1000+Drury+Lane,+Oakbrook+Terrace,+IL+60181/@41.859829,-87.9547293,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880e4c6048a69047:0xa8373ac49f00d007!2m2!1d-87.9513118!2d41.8601807!3e0' target='_blank'>Directions</a></button></p>
                 <p>To make a reservation please call </p>
-                <p className='Accommodations-phone'> 1-800-445-8667</p>
+                <b className='Accommodations-phone'> 1-800-445-8667</b>
                 <p>and mention the Soni & Pithadia Wedding</p>
             </div>
-            <img height="400" src={require("./media/acc/hotel.jpg")} alt="hotel" title="hotel" />
+            <img height="400" src={require("./media/acc/hotel.png")} alt="hotel" title="hotel" />
+        </div>
+        <div className='Accommodations-info'>
+            <div className='Accommodations-desc'>
+                <p>If you need additional help or have questions regarding bookings, you may contact</p>
+                <p>Amit Pithadia <b className='Accommodations-phone'> 1-800-445-8667</b> </p>
+                <p>Ravi Soni <b className='Accommodations-phone'> 1-800-445-8667</b></p>
+                <p>Tejas Kheradiya <b className='Accommodations-phone'> 1-800-445-8667</b></p>
+            </div>
         </div>
     </div>    
 );
--- src/WeddingParty.js
+++ src/WeddingParty.js
@@ -9,30 +9,30 @@ export const WeddingParty = () => (
             <div className='WeddingParty-desc'>
                 <h2 className='WeddingParty-info-section'>Rina's Family</h2>
                 <div className='WeddingParty-img'><a href="" title=""><img height="400" src={require("./media/weddingparty/soni-family.jpg")} alt="Rina's Parents" title="Rina's Parents" /></a></div>
-                <p className='WeddingParty-name'>Parents: Mukesh & Soni</p>
+                <p className='WeddingParty-name'>Parents: Mukesh & Bharati Soni</p>
                 <p className='WeddingParty-name'>Brother: Ravi Soni</p>
 
                 <h2 className='WeddingParty-info-section'>Rina's Bridesmaids</h2>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
+                <p className='WeddingParty-name'>Charmaine Lindley</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
+                <p className='WeddingParty-name'>Dipa Patel</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Elizabeth" title="Elizabeth"/></a></div> */}
                 <p className='WeddingParty-name'>Elizabeth George</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Jainy" title="Jainy"/></a></div> */}
                 <p className='WeddingParty-name'>Jainy Vathikulam</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
-                <p className='WeddingParty-name'>BM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
-                <p className='WeddingParty-name'>BM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
-                <p className='WeddingParty-name'>BM</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Nisha" title="Nisha"/></a></div> */}
                 <p className='WeddingParty-name'>Nisha Garg</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Pranali" title="Pranali"/></a></div> */}
-                <p className='WeddingParty-name'>Pranali Seth</p>
+                <p className='WeddingParty-name'>Pranali Sheth</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="BM" title="BM"/></a></div> */}
+                <p className='WeddingParty-name'>Radhika Desai</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Reena" title="Reena"/></a></div> */}
                 <p className='WeddingParty-name'>Reena Emmanuel</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Simi" title="Simi"/></a></div> */}
-                <p className='WeddingParty-name'>Simi Mathew</p>
                 {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Shreya" title="Shreya"/></a></div> */}
                 <p className='WeddingParty-name'>Shreya Dadhaniya</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Simi" title="Simi"/></a></div> */}
+                <p className='WeddingParty-name'>Simi Mathew</p>
 
             </div>
             <div className='WeddingParty-desc'>
@@ -43,24 +43,26 @@ export const WeddingParty = () => (
                 <p className='WeddingParty-name'>Brother: Sagar Pithadia </p>
 
                 <h2 className='WeddingParty-info-section'>Hiral's Groomsmen</h2>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=" "><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
-                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="GM" title="GM"/></a></div> */}
-                <p className='WeddingParty-name'>GM</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Adam Michael" title="Adam Michael"/></a></div> */}
+                <p className='WeddingParty-name'>Adam Michael</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Anshul Mehta" title="Anshul Mehta"/></a></div> */}
+                <p className='WeddingParty-name'>Anshul Mehta</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Colin Coelho" title="Colin Coelho"/></a></div> */}
+                <p className='WeddingParty-name'>Colin Coelho</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Deepak Bhandarkar" title="Deepak Bhandarkar"/></a></div> */}
+                <p className='WeddingParty-name'>Deepak Bhandarkar</p>
+                {/* <div className='WeddingParty-img'><a href="" title=" "><img height="300" src={require()} alt="James Sears" title="James Sears"/></a></div> */}
+                <p className='WeddingParty-name'>James Sears</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Kush Patel" title="Kush Patel"/></a></div> */}
+                <p className='WeddingParty-name'>Kush Patel</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Kush Sharma" title="Kush Sharma"/></a></div> */}
+                <p className='WeddingParty-name'>Kush Sharma</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Neil Bhatt" title="Neil Bhatt"/></a></div> */}
+                <p className='WeddingParty-name'>Neil Bhatt</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Rohit Maramraju" title="Rohit Maramraju"/></a></div> */}
+                <p className='WeddingParty-name'>Rohit Maramraju</p>
+                {/* <div className='WeddingParty-img'><a href="" title=""><img height="300" src={require()} alt="Sajan Shah" title="Sajan Shah"/></a></div> */}
+                <p className='WeddingParty-name'>Sajan Shah</p>
             </div>
         </div>
     </div>
