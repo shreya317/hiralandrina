d2e2f20fd2c85faedcbdcc2f711441735c622101
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2019-02-24 15:24:41 -0600

    update

--- src/Photos.js
+++ src/Photos.js
@@ -2,36 +2,37 @@ import React from 'react';
 import Gallery from 'react-grid-gallery';
 //CSS
 import './styles/Photos.css'
-import {ENGAGEMENT_CERMEONY, PROPOSAL_IMAGES} from './galleryPhotos';
+import {ENGAGEMENT_SHOOT, PROPOSAL_IMAGES} from './galleryPhotos';
 
 
 export const Photos = () => (
     <div className='Photos'>
-        <h1 className='Photos-title'>Photos</h1>
-        <h2 className='Photos-info-section'>Engagement Shoot</h2>
-        <h2 className='Photos-info-section'>Engagement Ceremony </h2>
-        <div className='Photos-gallery'>
-            <Gallery 
-                id={'Photos-gallery'}
-                images={ENGAGEMENT_CERMEONY}
-                enableImageSelection={false}
-                backdropClosesModal={true}
-                showLightboxThumbnails={true}
-                rowHeight={250}
-                maxRows={1}
-            />
-        </div>
-        <h2 className='Photos-info-section'>US</h2>
-        <div className='Photos-gallery'>
-            <Gallery 
-                id={'Photos-gallery'}
-                images={PROPOSAL_IMAGES}
-                enableImageSelection={false}
-                backdropClosesModal={true}
-                showLightboxThumbnails={true}
-                rowHeight={250}
-                maxRows={1}
-            />
+        <div className='Photos-container'>
+            <h1 className='Photos-title'>Photos</h1>
+            <h2 className='Photos-info-section'>Engagement Shoot</h2>
+            <div className='Photos-gallery'>
+                <Gallery 
+                    id={'Photos-gallery'}
+                    images={ENGAGEMENT_SHOOT}
+                    enableImageSelection={false}
+                    backdropClosesModal={true}
+                    showLightboxThumbnails={true}
+                    rowHeight={250}
+                    maxRows={1}
+                />
+            </div>
+            <h2 className='Photos-info-section'>US</h2>
+            <div className='Photos-gallery'>
+                <Gallery 
+                    id={'Photos-gallery'}
+                    images={PROPOSAL_IMAGES}
+                    enableImageSelection={false}
+                    backdropClosesModal={true}
+                    showLightboxThumbnails={true}
+                    rowHeight={250}
+                    maxRows={1}
+                />
+            </div>
         </div>
     </div>    
 );
--- src/galleryPhotos.js
+++ src/galleryPhotos.js
@@ -1,12 +1,153 @@
-export const ENGAGEMENT_CERMEONY = [
+export const ENGAGEMENT_SHOOT = [
     {
-        src: require('./media/gallery/IMG_8783.jpg'),
-        thumbnail: require('./media/gallery/IMG_8783.jpg'),
-        thumbnailWidth: 777,
-        thumbnailHeight: 1014
+        src: require('./media/e-shoot/RH-62.jpg'),
+        thumbnail: require('./media/e-shoot/RH-62.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
     },
-    
+    {
+        src: require('./media/e-shoot/RH-90.jpg'),
+        thumbnail: require('./media/e-shoot/RH-90.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-29.jpg'),
+        thumbnail: require('./media/e-shoot/RH-29.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
+    {
+        src: require('./media/e-shoot/RH-95.jpg'),
+        thumbnail: require('./media/e-shoot/RH-95.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-27.jpg'),
+        thumbnail: require('./media/e-shoot/RH-27.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-80.jpg'),
+        thumbnail: require('./media/e-shoot/RH-80.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-2.jpg'),
+        thumbnail: require('./media/e-shoot/RH-2.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
+    {
+        src: require('./media/e-shoot/RH-11.jpg'),
+        thumbnail: require('./media/e-shoot/RH-11.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-12.jpg'),
+        thumbnail: require('./media/e-shoot/RH-12.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-92.jpg'),
+        thumbnail: require('./media/e-shoot/RH-92.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
+    {
+        src: require('./media/e-shoot/RH-15.jpg'),
+        thumbnail: require('./media/e-shoot/RH-15.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-16.jpg'),
+        thumbnail: require('./media/e-shoot/RH-16.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-100.jpg'),
+        thumbnail: require('./media/e-shoot/RH-100.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
+    {
+        src: require('./media/e-shoot/RH-17.jpg'),
+        thumbnail: require('./media/e-shoot/RH-17.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-20.jpg'),
+        thumbnail: require('./media/e-shoot/RH-20.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-24.jpg'),
+        thumbnail: require('./media/e-shoot/RH-24.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-30.jpg'),
+        thumbnail: require('./media/e-shoot/RH-30.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-39.jpg'),
+        thumbnail: require('./media/e-shoot/RH-39.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-50.jpg'),
+        thumbnail: require('./media/e-shoot/RH-50.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-86.jpg'),
+        thumbnail: require('./media/e-shoot/RH-86.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-101.jpg'),
+        thumbnail: require('./media/e-shoot/RH-101.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
+    {
+        src: require('./media/e-shoot/RH-4.jpg'),
+        thumbnail: require('./media/e-shoot/RH-4.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    }, 
+    {
+        src: require('./media/e-shoot/RH-102.jpg'),
+        thumbnail: require('./media/e-shoot/RH-102.jpg'),
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+    },
 ]
+
+// export const ENGAGEMENT_CERMEONY = [
+//     {
+//         src: require('./media/gallery/IMG_8783.jpg'),
+//         thumbnail: require('./media/gallery/IMG_8783.jpg'),
+//         thumbnailWidth: 777,
+//         thumbnailHeight: 1014
+//     },
+
+// ]
 
 export const PROPOSAL_IMAGES = [
     {
@@ -90,6 +231,12 @@ export const PROPOSAL_IMAGES = [
         thumbnailHeight: 888
     },
     {
+        src: require('./media/gallery/IMG_8783.jpg'),
+        thumbnail: require('./media/gallery/IMG_8783.jpg'),
+        thumbnailWidth: 777,
+        thumbnailHeight: 1014
+    },
+    {
         src: require('./media/gallery/IMG_19.jpg'),
         thumbnail: require('./media/gallery/IMG_19.jpg'),
         thumbnailWidth: 320,
@@ -100,7 +247,8 @@ export const PROPOSAL_IMAGES = [
         thumbnail: require('./media/gallery/IMG_11.jpg'),
         thumbnailWidth: 888,
         thumbnailHeight: 888,
-    },    {
+    },    
+    {
         src: require('./media/gallery/IMG_12.jpg'),
         thumbnail: require('./media/gallery/IMG_12.jpg'),
         thumbnailWidth: 888,
