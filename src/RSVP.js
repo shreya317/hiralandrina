958e6b0e2a5c55092cbb8ff32d2584c0fa6a62b0
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2019-03-10 16:03:38 -0500

    rsvp

--- src/RSVP.js
+++ src/RSVP.js
@@ -4,9 +4,15 @@ import './styles/RSVP.css'
 
 export const RSVP = () =>( 
     <div className='Rsvp'>
-        {/* <h1 className='Rsvp-title'>Rsvp</h1> */}
-        <div className='Rsvp-comingsoon'>
-            <h1>RSVP coming soon...</h1>
+        <h1 className='Rsvp-title'>R.S.V.P.</h1>
+        <div className='Rsvp-container'>
+            <div className='Rsvp-info'>
+                <p>We look forward to celebrating with you!</p>
+                <p>To R.S.V.P., please email</p>
+                <p><b><a href = "mailto: RinaHiral2019@gmail.com">RinaHiral2019@gmail.com</a></b></p>
+                <p>with the events and the names of guests that will be attending.</p>
+                <p>Thank you!</p>
+            </div>
         </div>
     </div>
 );
