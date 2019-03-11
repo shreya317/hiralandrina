258a7f9ac1c8843bf85198418e1cbfaac2673a0f
Shreya Patel <shreyapatel@Shreya-Patels-MacBook-Pro.local>
2019-03-11 12:00:29 -0500

    countdown

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import './styles/Home.css'
 
-const getCountDownDays = () => {
-    const weddingDay = new Date('July 6, 2019').getTime();
-    const today = new Date();
-    const distance = weddingDay - today;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-    return days
-}
-
 export default class Home extends Component {
     render () {
         return (
@@ -18,7 +9,8 @@ export default class Home extends Component {
                     <div className='Home-info'>
                         <h1 className='Home-names'>Rina & Hiral</h1>
                         <h3 className='Home-date'>July 6th, 2019 • Oakbrook Terrace, IL</h3>
-                        <h3 className='Home-countdown'>{getCountDownDays()} days to go!</h3>
+                        <div className='Home-cd-normal'><iframe src="http://free.timeanddate.com/countdown/i6o4fjb8/n64/cf12/cm0/cu4/ct0/cs0/ca0/co0/cr0/ss0/cacfff/cpc000/pct/tcfff/fn2/fs100/szw576/szh243/iso2019-07-06T00:00:00" allowTransparency="true" frameborder="0" width="161" height="49"></iframe></div>
+                        <div className='Home-cd-mobile'><iframe src="http://free.timeanddate.com/countdown/i6o4fjb8/n64/cf12/cm0/cu4/ct0/cs0/ca0/co0/cr0/ss0/cacfff/cpc000/pct/tcfff/fn2/fs100/szw320/szh135/iso2019-07-06T00:00:00" allowTransparency="true" frameborder="0" width="96" height="28"></iframe></div>
                     </div>
                 </div>
             </div>
